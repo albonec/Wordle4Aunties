00ab0171dc0cc60963a3491db7994957f77b332c
albonec <agbonecchi@gmail.com>
2023-03-12 11:04:26 -0400

    anticheat added for ppls who like to break stuff

--- Wordle.java
+++ Wordle.java
@@ -190,7 +190,13 @@ public class Wordle {
             lines.add(scan.nextLine());
         }
         for (int i = 0; i < lines.size(); i++) {
-            scoreOutput[Integer.valueOf(lines.get(i)) - 1] += 1;
+            if(Integer.valueOf(lines.get(i)) < 6 && Integer.valueOf(lines.get(i)) - 1 >= 0) {
+                scoreOutput[Integer.valueOf(lines.get(i)) - 1] += 1;
+            } else {
+                wipeScores();
+                Arrays.fill(scoreOutput, 0);
+                throw new IllegalArgumentException("Value too high! Minimum number of guesses for a win is 1, maximum is 6. Scores have been wiped!");
+            }
         }
         return scoreOutput;
     }
